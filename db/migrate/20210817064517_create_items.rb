1e44c0b1512cb6abe817702268e79115244b5fc7
Kinocas <717koushi717@gmail.com>
2021-08-17 16:03:52 +0900

    マイグレーションファイル修正

--- db/migrate/20210817064517_create_items.rb
+++ db/migrate/20210817064517_create_items.rb
@@ -8,7 +8,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :fee_burden_id, null: false 
       t.integer :prefecture_id, null: false 
       t.integer :day_id,        null: false 
-      t.reference :user,        null: false, foreign_key: true
+      t.references :user,        null: false, foreign_key: true
 
       t.timestamps
     end
