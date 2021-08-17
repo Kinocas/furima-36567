be7235d3a0e98d1f5bf03bd4fa8615582da85cea
Kinocas <717koushi717@gmail.com>
2021-08-17 18:39:16 +0900

    商品画像から詳細まで実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,18 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :category_id , presence: true
-  validates :status_id, presence: true
+  validates :category_id , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :fee_burden_id, presence: true
   validates :prefecture_id, presence: true
   validates :day_id, presence: true
 
   belongs_to :user
   has_one :purchase 
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :Status
 
 end
