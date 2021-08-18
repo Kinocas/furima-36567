92a46bbba2fe7e43f6b585a66d22c429513436de
Kinocas <717koushi717@gmail.com>
2021-08-18 11:17:14 +0900

    価格の範囲設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   validates :fee_burden_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :day_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   belongs_to :user
   has_one :purchase 
