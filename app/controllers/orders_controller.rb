911954240152a9bc5dd37f4248c446bd6bef4681
Kinocas <717koushi717@gmail.com>
2021-08-20 16:42:49 +0900

    購入履歴の保存機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,12 +2,24 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-  end
-
-  def new
+    @order_shopping = OrderShopping.new
   end
 
   def create
+    @order_shopping = OrderShopping.new(order_params)
+    if @order_shopping.valid?
+      @order_shopping.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shopping).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
